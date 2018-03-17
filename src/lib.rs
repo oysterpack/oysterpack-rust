d71d782a6555d429b1398d41cfa11c973f9b3391
Alfio Zappala <oysterpack2016@gmail.com>
2018-03-17 09:20:58 -0400

    updated documention

--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! OysterPack Rust Platform
+//! # OysterPack Rust Reactive Platform
 //!
+//! The mission is to provide a platform to build [reactive](https://www.reactivemanifesto.org/)
+//! systems in Rust.
+//!
+//! ## Why Rust ?
+//! Because [Rust](https://www.rust-lang.org) is the best systems programming language for building
+//! production grade reactive systems today. It's main competitive advantages are:
+//!
+//! 1. Bare metal performance
+//! 2. Memory safety features guarantees with out the need for a garbage collector
+//!    - garbage collectors add resource overhead and add to bloat
+//! 3. Small memory footprint
+//!    - no language runtime required
+//!    - no garbage collector
+//! 4. Rust compiler
+//!    - prevents segfaults
+//!    - prevents data races
+//!    - smartest compiler in the market that detects runtime issues at compile time
+//!    - compiler is designed to help you figure out what went wrong and provides suggestions
+//! 5. Cargo tooling and Rust ecosystem
+//!    - simple to learn and use
+//!    - promotes collaboration and productivity
+//!    - [crates.io](https://crates.io/)
+//!    - makes it easy to build and publish crates
+//!
+
+pub mod messages;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn quick_test() {
     }
 }
