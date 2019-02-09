283716e40b54cedb7469cb2cd084cf17c8723578
Alfio Zappala <oysterpack2016@gmail.com>
2019-02-09 11:30:04 -0500

    oysterpack-trust - migrated reqrep_bench to criterion

--- oysterpack-trust/benches/reqrep_bench.rs
+++ oysterpack-trust/benches/reqrep_bench.rs
@@ -16,39 +16,17 @@
 
 //! request/reply messaging bench tests
 
-/*
-Test Results
-====================================================================================================
-[INFO][2019-02-09T15:22:44.425Z][reqrep_function_single_threaded_sync_baseline][reqrep_bench:238]
-*** count = 100000, duration = 151ns, ns/req = 0
-[INFO][2019-02-09T15:22:44.427Z][reqrep_function_single_threaded_baseline][reqrep_bench:245]
-*** count = 100000, duration = 1.459436ms, ns/req = 14
-[INFO][2019-02-09T15:22:45.580Z][reqrep_bench_single_threaded][reqrep_bench:252]
-*** count = 100000, duration = 1.152367149s, ns/req = 11523
-[INFO][2019-02-09T15:22:45.659Z][reqrep_function_multi_threaded_baseline][reqrep_bench:259]
-*** count = 100000, duration = 78.94042ms, ns/req = 789
-[INFO][2019-02-09T15:22:45.853Z][reqrep_bench_multi_threaded][reqrep_bench:266]
-*** count = 100000, duration = 186.815473ms, ns/req = 1868
-[INFO][2019-02-09T15:22:45.853Z][reqrep_bench metrics][reqrep_bench:271]
-====================================================================================================
-Analysis
-- the async overhead is ~7650x as compared to calling the function directly
-- using channels for req/rep was ~1000x slower as compared to calling plain old function
-- when using channels, multi-threaded request sending increased throughput ~7x - which aligns to the
-  8 cores on my computer
-
-Summary
-- the above benchmarks are looking at pure CPU bound throughput. In real world networked apps, the main
-  advantage comes from non-blocking async IO
-- even so, because we are benchmarking at such a micro scale, the overall Futures overhead is acceptable
-*/
-
 #![feature(await_macro, async_await, futures_api, arbitrary_self_types)]
 #![allow(warnings)]
 
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+
 use oysterpack_trust::{
     concurrent::{
-        execution::*,
+        execution::{self, *},
         messaging::reqrep::{self, *},
     },
     metrics,
@@ -61,12 +39,49 @@ use futures::{
     stream::StreamExt,
     task::{Spawn, SpawnExt},
 };
+use lazy_static::lazy_static;
 use oysterpack_log::*;
 use std::{
     pin::Pin,
+    sync::{Arc, Mutex},
     thread,
     time::{Duration, Instant},
 };
+
+criterion_group!(benches, reqrep_bench_bench,);
+
+criterion_main!(benches);
+
+lazy_static! {
+    static ref CLIENT: Arc<Mutex<ReqRep<(), ()>>> = {
+        let req_rep = ReqRep::start_service(
+            ReqRepId::generate(),
+            1,
+            EchoService,
+            ExecutorBuilder::new(ExecutorId::generate())
+                .register()
+                .unwrap(),
+            timer_buckets(),
+        )
+        .unwrap();
+        Arc::new(Mutex::new(req_rep))
+    };
+}
+
+/// measures how long a request/reply message flow takes
+fn reqrep_bench_bench(c: &mut Criterion) {
+    c.bench_function("reqrep_bench_bench", move |b| {
+        let mut executor = global_executor();
+        b.iter(|| {
+            executor.run(
+                async {
+                    let mut req_rep = CLIENT.lock().unwrap();
+                    await!(req_rep.send(()))
+                },
+            );
+        })
+    });
+}
 
 struct EchoService;
 
@@ -91,184 +106,4 @@ fn timer_buckets() -> metrics::TimerBuckets {
         ]
         .as_slice(),
     )
-}
-
-/// This is benchmarking how fast messages can flow in a request/reply workflow.
-/// - the request message is imply echoed back, and the message type is ()
-///
-/// avg response time ~11 microseconds
-fn reqrep_bench_single_threaded(count: usize) -> Duration {
-    let mut executor = global_executor();
-    let req_rep = ReqRep::start_service(
-        ReqRepId::generate(),
-        1,
-        EchoService,
-        executor.clone(),
-        timer_buckets(),
-    )
-    .unwrap();
-
-    let req_rep_1 = req_rep.clone();
-    let clock = quanta::Clock::new();
-    let start = clock.start();
-    executor.run(
-        async move {
-            let mut req_rep = req_rep_1;
-            for _ in 0..count {
-                let rep_receiver = await!(req_rep.send(())).unwrap();
-                await!(rep_receiver.recv()).unwrap();
-            }
-        },
-    );
-    let end = clock.end();
-    Duration::from_nanos(clock.delta(start, end))
-}
-
-/// This is benchmarking message throughput in a request/reply workflow running parallel tasks
-/// - the request message is imply echoed back, and the message type is ()
-/// - sending messages is multi-threaded, but requests are still processed by a single listener
-/// - throughput increased ~7x
-///
-/// avg response time ~1.8 microseconds
-fn reqrep_bench_multi_threaded(count: usize) -> Duration {
-    let mut executor = global_executor();
-    let req_rep = ReqRep::start_service(
-        ReqRepId::generate(),
-        num_cpus::get(),
-        EchoService,
-        executor.clone(),
-        timer_buckets(),
-    )
-    .unwrap();
-
-    let mut handles = Vec::with_capacity(count);
-    let clock = quanta::Clock::new();
-    let start = clock.start();
-    for i in 1..=count {
-        let req_rep_1 = req_rep.clone();
-        let task = async move {
-            let mut req_rep = req_rep_1;
-            let rep_receiver = await!(req_rep.send(())).unwrap();
-            await!(rep_receiver.recv()).unwrap();
-        };
-        let handle = executor.spawn_with_handle(task).unwrap();
-        handles.push(handle);
-    }
-    executor.run(
-        async move {
-            await!(join_all(handles));
-        },
-    );
-    let end = clock.end();
-    Duration::from_nanos(clock.delta(start, end))
-}
-
-// avg response time ~11 nanos
-fn reqrep_function_single_threaded_baseline(count: usize) -> Duration {
-    let mut executor = global_executor();
-    let f = |msg| Result::<_, ()>::Ok(msg);
-
-    let clock = quanta::Clock::new();
-    let start = clock.start();
-    executor.run(
-        async move {
-            for _ in 0..count {
-                await!(async { f(()) }).unwrap();
-            }
-        },
-    );
-    let end = clock.end();
-    Duration::from_nanos(clock.delta(start, end))
-}
-
-// avg response time ~11 nanos
-fn reqrep_function_single_threaded_sync_baseline(count: usize) -> Duration {
-    let f = |msg| Result::<_, ()>::Ok(msg);
-
-    let clock = quanta::Clock::new();
-    let start = clock.start();
-    for _ in 0..count {
-        f(());
-    }
-    let end = clock.end();
-    Duration::from_nanos(clock.delta(start, end))
-}
-
-/// ~750 nanos - which means there was a lot of overhead added as compared to the single threaded
-/// baseline that wasn't made up with parallel processing
-/// - overall throughput decreased by ~40x
-fn reqrep_function_multi_threaded_baseline(count: usize) -> Duration {
-    let mut executor = global_executor();
-
-    let f = |msg| Result::<_, ()>::Ok(msg);
-    let mut handles = Vec::with_capacity(count);
-    let clock = quanta::Clock::new();
-    let start = clock.start();
-    for i in 1..=count {
-        let task = async move {
-            await!(async { f(()) }).unwrap();
-        };
-        let handle = executor.spawn_with_handle(task).unwrap();
-        handles.push(handle);
-    }
-    executor.run(
-        async move {
-            for handle in handles {
-                let _ = await!(handle);
-            }
-        },
-    );
-    let end = clock.end();
-    Duration::from_nanos(clock.delta(start, end))
-}
-
-fn log_config() -> oysterpack_log::LogConfig {
-    oysterpack_log::config::LogConfigBuilder::new(oysterpack_log::Level::Info)
-        .target_level(
-            oysterpack_log::Target::from(env!("CARGO_PKG_NAME")),
-            oysterpack_log::Level::Warn,
-        )
-        .build()
-}
-
-fn main() {
-    oysterpack_log::init(log_config(), oysterpack_log::StderrLogger);
-    let count = 100000;
-
-    let duration = reqrep_function_single_threaded_sync_baseline(count);
-    let nanos_per_req = duration.as_nanos() / (count as u128);
-    log!(target: "reqrep_function_single_threaded_sync_baseline", Level::Info,
-         "*** count = {}, duration = {:?}, ns/req = {}",
-         count, duration, nanos_per_req
-    );
-
-    let duration = reqrep_function_single_threaded_baseline(count);
-    let nanos_per_req = duration.as_nanos() / (count as u128);
-    log!(target: "reqrep_function_single_threaded_baseline", Level::Info,
-        "*** count = {}, duration = {:?}, ns/req = {}",
-        count, duration, nanos_per_req
-    );
-
-    let duration = reqrep_bench_single_threaded(count);
-    let nanos_per_req = duration.as_nanos() / (count as u128);
-    log!(target: "reqrep_bench_single_threaded", Level::Info,
-        "*** count = {}, duration = {:?}, ns/req = {}",
-        count, duration, nanos_per_req
-    );
-
-    let duration = reqrep_function_multi_threaded_baseline(count);
-    let nanos_per_req = duration.as_nanos() / (count as u128);
-    log!(target: "reqrep_function_multi_threaded_baseline", Level::Info,
-        "*** count = {}, duration = {:?}, ns/req = {}",
-        count, duration, nanos_per_req
-    );
-
-    let duration = reqrep_bench_multi_threaded(count);
-    let nanos_per_req = duration.as_nanos() / (count as u128);
-    log!(target: "reqrep_bench_multi_threaded", Level::Info,
-        "*** count = {}, duration = {:?}, ns/req = {}",
-        count, duration, nanos_per_req
-    );
-
-    log!(target: "reqrep_bench metrics", Level::Info, "{:#?}", metrics::registry().gather());
 }
